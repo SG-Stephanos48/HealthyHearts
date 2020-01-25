96d59283d0d856d3b1d5b732e487877b2206711e
Stephen English <stephanos.english@gmail.com>
2020-01-25 15:17:41 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HealthyHearts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+
+            Console.WriteLine("What is your age? ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            CalculateHeartRate(age);
+        }
+
+        public static void CalculateHeartRate(int age)
+        {
+            int heartrate;
+            double lowzone;
+            double highzone;
+
+            heartrate = 220 - age;
+            lowzone = heartrate * .50;
+            highzone = heartrate * .85;
+
+            Console.WriteLine("Your maximum Heart Rate is: " + heartrate);
+            Console.WriteLine("Your target Heart Rate Zone is " + lowzone + " - " + highzone + " beats per minute");
+        }
+    }
+}
